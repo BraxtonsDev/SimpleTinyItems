5963ae1d17a2b8ee965f43314a3cd3592629f372
Braxton W <supahninja10@gmail.com>
2025-12-22 22:06:42 -0800

    Initial commit

---
+++ src/main/java/brax/simpletinyitems/SimpleTinyItemsClient.java
@@ -0,0 +1,11 @@
+package brax.simpletinyitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleTinyItemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
